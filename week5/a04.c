930faf954f3ff583cc86dd5cac4a6a4955a28cb1
Amitava <dattaarup2004@gmail.com>
2025-02-26 21:15:57 +0530

    week 5 assignment

---
+++ week5/a04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    do
+    {
+        printf("while vs do-while\n");
+    } while (i == 1);
+    printf("Out of loop");
+    return 0;
+}
---
+++ week5/a06.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0, i, b;
+    for (i = 0; i <= 2; i += 0.5)
+    {
+        a++;
+        continue;
+    }
+    printf("%d", a);
+    return 0;
+}
